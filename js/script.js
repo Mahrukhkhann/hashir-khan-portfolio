ab122370576211f127ffc5e5dc65b0ab8c1d99d4
Mahrukhkhann <mahrukh.khan.official@gmail.com>
2026-08-07 17:53:23 +0500

    change timeline

--- js/script.js
+++ js/script.js
@@ -317,3 +317,27 @@ window.addEventListener("load",()=>{
     document.querySelector(".loader").style.display="none";
 
 });
+// timeline
+const timelineItems = document.querySelectorAll(".timeline-item");
+
+const timelineObserver = new IntersectionObserver((entries)=>{
+
+    entries.forEach(entry=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+},{
+    threshold:0.3
+});
+
+timelineItems.forEach(item=>{
+
+    timelineObserver.observe(item);
+
+});
